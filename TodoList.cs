6b1a29f1b171e59ee4931ee2f527ea386b36cc5a
Ilham Alfayyad <ilham.alfayyad@gmail.com>
2025-01-11 12:38:46 +0100

    Logiken.Class TodoList

---
+++ TodoList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTodoApp
+{
+    /// <summary>
+    /// hanterar logiken för lägga till, ta bort och visa todo items
+    /// </summary>
+    public class TodoList
+    {
+        private List<string> tasks = new List<string>();
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+
+                tasks.RemoveAt(index);
+            }
+        }
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
+    }
+}
