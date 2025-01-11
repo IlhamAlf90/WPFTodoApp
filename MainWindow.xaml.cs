66550c3b2e9e06e29f36ced0464dea1a294f32fe
Ilham Alfayyad <ilham.alfayyad@gmail.com>
2025-01-11 13:14:08 +0100

    Logisk update. Main Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFTodoApp
 {
@@ -16,18 +7,54 @@ namespace WPFTodoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Lista med todo items
+        /// </summary>
+        private TodoList _todoList;
         public MainWindow()
         {
             InitializeComponent();
+            _todoList = new TodoList();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todoList.AddTask(task);
+                UpdateTaskList();
+                TaskTextBox.Clear();
+            }
         }
 
+        /// <summary>
+        /// updaterar list med todo items
+        /// </summary>
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todoList.GetAllTasks())
+            {
+                TasksListBox.Items.Add(task);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _todoList.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
+            }
 
         }
     }
